92a631029cb0a434ce1630eb75a61ae962751c98
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-22 23:58:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -142,9 +142,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+binding.pry
     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
     
     programmer_hash
-
+    
 end
